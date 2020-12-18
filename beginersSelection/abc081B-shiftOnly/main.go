331e3f89b3464e0b7614117d2720b7237dadca5e
souhub <ace0804sou@gmail.com>
2020-12-19 01:26:25 +0900

    Tryied shift-only

--- beginersSelection/abc081B-shiftOnly/main.go
+++ beginersSelection/abc081B-shiftOnly/main.go
@@ -3,7 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	var N int
+	var N, count int
+	count = 0
+
 	fmt.Scanf("%d", &N)
 
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Scanf("%d", &A[i])
+	}
+
+	for {
+		existAdd := false
+
+		for i := 0; i < N; i++ {
+			if A[i]%2 != 0 {
+				existAdd = true
+			}
+		}
+
+		if existAdd {
+			break
+		}
+
+		for i := 0; i < N; i++ {
+			A[i] /= 2
+		}
+		count++
+	}
+
+	println(count)
 }
