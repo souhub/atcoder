49c22ce6cc44aa3e966946501e6f7b5be2311d65
souhub <ace0804sou@gmail.com>
2020-12-22 16:03:01 +0900

    Tried to answer ABC086A by python

---
+++ beginersSelection/abc086A-product/main.py
@@ -0,0 +1,7 @@
+a, b = map(int, input().split())
+r = a*b
+
+if a*b % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
--- playground/main.go
+++ playground/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	// var S string
-	// fmt.Scanf("%s", &S)
-	s := "Helloworld"
-	fmt.Println(string(s[4]))
-
+	var ix, iy, sin, cos, tan float64
+	x := 0.0
+	y := 1.0
+	rx := (x-1.0)*(x-1.0) + y*y
+	ix = -(x-1)*(x-1) - y*y + 3*(x-1)*(x-1)*math.Pow(rx, -2.5)
+	iy = 3 * y * (x - 1) * math.Pow((x-1)*(x-1)+y*y, -2.5)
+	r := math.Sqrt(ix*ix + iy*iy)
+	sin = math.Sin(iy / r)
+	cos = math.Cos(ix / r)
+	tan = sin / cos
+	fmt.Printf("(x,y)=(%v.%v)のとき\nix=%v\niy=%v\ntanθ=%v\n", x, y, ix, iy, tan)
 }
 
 func reverse(s string) string {
